6613941c77697c5d87890ce1be60308fb70ab06c
Sai Harsha <134918367+rSaiHarsha@users.noreply.github.com>
2024-11-14 09:40:14 +0530

    bug-fix third commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,62 +1,49 @@
-
-// import React  from "react";
-// import PdfUpload from "./components/PdfUpload";
-// import io from 'socket.io-client'
-// function App() {
-//   const socket = io('http://localhost:5000');
-
-//   const sendMessage = function () {
-//       socket.emit("client-msg","this is from client")
-//   }
-//   return (
-//     <div className="App">
-//       <button onClick={sendMessage}>send msg</button>
-//       <br />
-//       <PdfUpload/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// frontend/src/App.js
-
 import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PdfUpload from "./components/PdfUpload";
-import io from 'socket.io-client';
+import PdfComp from "./components/PdfComp";
+
+const socket = new WebSocket("ws://localhost:5000"); // WebSocket connection to the backend
 
 function App() {
-  // Connect to the backend server with Socket.IO
-  const socket = io('http://localhost:5000');
-
-  // Send a message to the server
-  const sendMessage = function () {
-    socket.emit("client-msg", "this is from client");
-  };
+  // Send a message to the server (example function)
+  // const sendMessage = function () {
+  //   const message = JSON.stringify({ type: "client-msg", message: "this is from client" });
+  //   socket.send(message); // Send message to server
+  // };
 
   // Log connection status and handle cleanup
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log('Connected to the server');
-    });
-
-    socket.on('disconnect', () => {
-      console.log('Disconnected from server');
-    });
-
-    // Cleanup on component unmount
-    return () => {
-      socket.off('connect');
-      socket.off('disconnect');
+    socket.onopen = () => {
+      console.log("Connected to the WebSocket server");
     };
-  }, [socket]);
+
+    socket.onclose = () => {
+      console.log("Disconnected from WebSocket server");
+    };
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.type === "updatePage") {
+        console.log(`Page synced to: ${data.pageNumber}`);
+      }
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
 
   return (
-    <div className="App">
-      <button onClick={sendMessage}>Send Message to Server</button>
-      <br />
-      <PdfUpload />
-    </div>
+    <Router>
+      <div className="App">
+       
+        <Routes>
+          <Route path="/" element={<PdfUpload />} />
+          <Route path="/pdf-viewer" element={<PdfComp />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
